4309ecb0ca53c96f07079a18ab2e44a44c57db10
Piyatorn <piyatorn.boss@gmail.com>
2025-06-10 18:20:06 +0700

    fix : disable btn create and adjust

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('form').on('submit', function () {
+        if (this.checkValidity()) {
+            $(this).find('button.disable-on-submit').each(function() {
+                var button = $(this);
+                button.prop('disabled', true);
+                button.data('original-text', button.html());
+                button.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Processing...');
+            });
+        }
+    });
+});
